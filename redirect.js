07d2e30b99d78660a7a8d43ac2c510f8fd35785f
marija <mladendinc@hotmail.com>
2019-09-22 22:58:06 +0200

    Update 404

--- redirect.js
+++ redirect.js
@@ -1,62 +1,66 @@
-function $(expr, con) {
-	return typeof expr === 'string'? (con || document).querySelector(expr) : expr;
-}
+// function $(expr, con) {
+// 	return typeof expr === 'string'? (con || document).querySelector(expr) : expr;
+// }
 
-function $$(expr, con) {
-	return Array.prototype.slice.call((con || document).querySelectorAll(expr));
-}
+// function $$(expr, con) {
+// 	return Array.prototype.slice.call((con || document).querySelectorAll(expr));
+// }
 
-function xhr(o) {
-	var xhr = new XMLHttpRequest(o.src);
+// function xhr(o) {
+// 	var xhr = new XMLHttpRequest(o.src);
 	
-	xhr.open("GET", o.src);
+// 	xhr.open("GET", o.src);
 	
-	xhr.onreadystatechange = function () {
-		if (xhr.readyState == 4) {
-			if (xhr.status < 400) {
-				try {
-					o.onsuccess.call(xhr);
-				}
-				catch (e) {
-					o.onerror.call(xhr);
-					console.error(e);
-				}
-			}
-			else {
-				o.onerror.call(xhr);
-			}
-		}
-	};
+// 	xhr.onreadystatechange = function () {
+// 		if (xhr.readyState == 4) {
+// 			if (xhr.status < 400) {
+// 				try {
+// 					o.onsuccess.call(xhr);
+// 				}
+// 				catch (e) {
+// 					o.onerror.call(xhr);
+// 					console.error(e);
+// 				}
+// 			}
+// 			else {
+// 				o.onerror.call(xhr);
+// 			}
+// 		}
+// 	};
 	
-	xhr.send();
-}
+// 	xhr.send();
+// }
 
-(function(){
+// (function(){
 
-document.body.className = 'redirecting';
+// document.body.className = 'redirecting';
 
-var slug = location.pathname.slice(1);
+// var slug = location.pathname.slice(1);
 
-xhr({
-	src: 'secrets.json',
-	onsuccess: function () {
-		var slugs = JSON.parse(this.responseText);
+// xhr({
+// 	src: 'secrets.json',
+// 	onsuccess: function () {
+// 		var slugs = JSON.parse(this.responseText);
 		
-		var hash = slugs[slug];
+// 		var hash = slugs[slug];
 		
-		if (hash) {
-			// Redirect
-			var url = hash.indexOf('http') == 0? hash : 'http://mladendinic.com/gist/' + hash;
-			$('section.redirecting > p').innerHTML = 'Redirecting to <a href="' + url + '">' + url + '</a>…';
-			location.href = 'http://mladendinic.com/';
-		}
-		else {
-			document.body.className = 'error not-found';
-		}
-	},
-	onerror: function () {
-		//document.body.className = 'error json';
-	}
-});
+// 		if (hash) {
+// 			// Redirect
+// 			var url = hash.indexOf('http') == 0? hash : 'http://mladendinic.com/gist/' + hash;
+// 			$('section.redirecting > p').innerHTML = 'Redirecting to <a href="' + url + '">' + url + '</a>…';
+// 			location.href = 'http://mladendinic.com/';
+// 		}
+// 		else {
+// 			document.body.className = 'error not-found';
+// 		}
+// 	},
+// 	onerror: function () {
+// 		//document.body.className = 'error json';
+// 	}
+// });
 
-})();
+// })();
+
+function() {
+    document.location.href = "/";
+}
